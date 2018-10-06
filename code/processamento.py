de811d44ac124acacb55a1f2f1b54f54cbaa6716
Mateus Gündel <gundel5@mx2.unisc.br>
2018-10-06 19:47:42 -0300

    Não ta funcionando isso

--- code/processamento.py
+++ code/processamento.py
@@ -1,3 +1,6 @@
-# def valida_entrada():
-def to_words(programa):
-    return programa.split(" ")
+import passo1, passo2, passo3, passo4
+
+class validacoes():
+    # def valida_entrada():
+    def to_words(programa):
+        return programa.split(" ")
